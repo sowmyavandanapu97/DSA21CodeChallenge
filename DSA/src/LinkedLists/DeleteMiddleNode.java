011518fb5e80e2748e83d00ce7a1f22598839e17
sowmyavandanapu97 <sowmya.vandanapu.97@gmail.com>
2024-04-08 15:11:14 -0400

    DeleteMiddleNode and basic org

---
+++ DSA/src/LinkedLists/DeleteMiddleNode.java
@@ -0,0 +1,12 @@
+package LinkedLists;
+
+//Given a linked list, delete the middle node.
+//If the list is even length, delete the node at the start of 
+//the second half of the list.
+
+public class DeleteMiddleNode {
+
+	public static void main(String args[]) {
+
+	}
+}
